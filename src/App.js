a6f37525f844c526f634d3fd803b6728835a22ae
thaqien <thaqien.jusuf@gmail.com>
2021-01-10 16:42:32 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import { Button } from "@material-ui/core";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="secondary">Hello</Button>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
